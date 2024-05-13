fa6fcf7642d25ee9351fcc399aa581ae945902da
MasamuneDate99 <natdavin@gmail.com>
2024-05-13 15:26:25 +0700

    feat. Image Recognition (eng & id)

--- wallet-service/src/main/java/fullstuck/green/wallet/Service/TesseractOCRService.java
+++ wallet-service/src/main/java/fullstuck/green/wallet/Service/TesseractOCRService.java
@@ -1,2 +1,28 @@
-package fullstuck.green.wallet.service;public class TesseractOCRService {
+package fullstuck.green.wallet.Service;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Service
+public class TesseractOCRService {
+
+    @Autowired
+    private Tesseract tesseract;
+
+    public String recognizeText(InputStream inputStream) throws IOException {
+        BufferedImage image = ImageIO.read(inputStream);
+        try {
+            return tesseract.doOCR(image);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+        return "failed";
+    }
 }
