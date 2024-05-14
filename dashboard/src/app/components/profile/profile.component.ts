d4ecfcddd1dccd2ba7c2b8366a60808365d972f4
MuhamadFaheem <faheemf428@gmail.com>
2024-05-14 10:47:24 +0700

    refactor: fixing profile layout

--- dashboard/src/app/components/profile/profile.component.ts
+++ dashboard/src/app/components/profile/profile.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  opt: string|boolean = true;
+  navigateToUpdateProfile() {
+    this.opt = !this.opt
+  }
+  balance = '1000';
+  name = 'Clarke Jeffery';
   email = 'Cjg5N@example.com';
   phone = '1234567890';
+  bod = '01-01-1990';
 }
