1989fe82db5799772364269286c7c9a3dac450bc
jokourno12 <jokourno12@gmail.com>
2024-05-14 08:51:25 +0700

    Unit Testing for Topup

--- dashboard/src/app/components/topup/topup.component.spec.ts
+++ dashboard/src/app/components/topup/topup.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TopupComponent } from './topup.component';
 
-describe('TopupComponent', () => {
+fdescribe('TopupComponent', () => {
   let component: TopupComponent;
   let fixture: ComponentFixture<TopupComponent>;
 
@@ -17,7 +17,22 @@ describe('TopupComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(TopupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should copy text to clipboard', ()=>{
+    const mockText = 'test text';
+    spyOn(navigator.clipboard, 'writeText').and.returnValue(Promise.resolve());
+
+    component.copyText(mockText);
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(mockText);
+  });
+  
 });
