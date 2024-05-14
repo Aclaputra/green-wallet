2eba470401bff7e452190323abc66cace89722e4
gagaswahyuin <gagaswahyuin@gmail.com>
2024-05-14 15:42:05 +0700

    feat: add forgot-password

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { TransferComponent } from './components/transfer/transfer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+  },
+  {
+    path: 'transfer',
     component: TransferComponent,
   },
 ];
