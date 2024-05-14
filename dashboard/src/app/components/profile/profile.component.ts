eb22faa643d0006ca2e38fb6dace148e586d96ee
MuhamadFaheem <faheemf428@gmail.com>
2024-05-14 08:41:41 +0700

    feat: init profile component (without service)

---
+++ dashboard/src/app/components/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+  email = 'Cjg5N@example.com';
+  phone = '1234567890';
+}
