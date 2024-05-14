eaffe7bd9910fa02a0f0e69ca7a265846411fb12
MuhamadFaheem <faheemf428@gmail.com>
2024-05-14 08:48:41 +0700

    feat: add route ts

--- dashboard/src/app/app.routes.ts
+++ dashboard/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'profile',
+        title: 'Profile Page',
+        component: ProfileComponent
+    },
+    {
+        
+    }
+];
