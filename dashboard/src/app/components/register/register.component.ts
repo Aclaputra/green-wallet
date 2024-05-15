e8018a46bc3cf8c8335d3ba4b95e09dfa000ec29
MuhamadFaheem <faheemf428@gmail.com>
2024-05-15 11:51:56 +0700

    refactor: fixing register detail (adding toastr)

--- dashboard/src/app/components/register/register.component.ts
+++ dashboard/src/app/components/register/register.component.ts
@@ -1,19 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { RouterTestingModule } from "@angular/router/testing";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, RouterTestingModule],
+  imports: [RouterLink, ReactiveFormsModule,],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-  regist() {  
-    console.log(this.registerForm.value);
+  resp: any;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
+  regist() {
+    if(this.registerForm.valid){
+      this.http.post('http://localhost:8080/auth/register', this.registerForm.value).subscribe({
+        next: (data) => {
+          this.resp = data;
+          if(this.resp.statusCode == 201){
+            this.router.navigate(['/login']);
+            this.toastr.success('Registered successfully', 'Success');
+          }
+        },
+        error: (error) => {
+          if(error.status == 403){
+            this.toastr.error("Data already exists", 'Error'); 
+          }
+        },
+      })
+    }
   }
 
   registerForm = new FormGroup({
@@ -24,6 +47,5 @@ export class RegisterComponent {
     birthDate: new FormControl('', [Validators.required]),
     username: new FormControl('', [Validators.required]),
   })
-
 
 }
