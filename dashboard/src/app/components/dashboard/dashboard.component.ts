aa494a5ef32ba47de836c455400071a24ffab2ff
gagaswahyuin <gagaswahyuin@gmail.com>
2024-05-15 22:41:36 +0700

    feat: add chart

--- dashboard/src/app/components/dashboard/dashboard.component.ts
+++ dashboard/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,88 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, ChartModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  data: any;
+  options: any;
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue(
+      '--text-color-secondary'
+    );
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.data = {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+      ],
+      datasets: [
+        {
+          label: 'Income',
+          backgroundColor: documentStyle.getPropertyValue('--green-chart'),
+          borderColor: documentStyle.getPropertyValue('--green-chart'),
+          data: [65000, 59000, 80000, 81000, 56000, 55000, 40000],
+        },
+        {
+          label: 'Outcome',
+          backgroundColor: documentStyle.getPropertyValue('--red-chart'),
+          borderColor: documentStyle.getPropertyValue('--red-chart'),
+          data: [28000, 48000, 40000, 19000, 86000, 27000, 90000],
+        },
+      ],
+    };
+
+    this.options = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.8,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor,
+          },
+        },
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary,
+            font: {
+              weight: 500,
+            },
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+        y: {
+          ticks: {
+            color: textColorSecondary,
+          },
+          grid: {
+            color: surfaceBorder,
+            drawBorder: false,
+          },
+        },
+      },
+    };
+  }
+}
